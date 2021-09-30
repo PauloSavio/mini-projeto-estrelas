c7963e903b1c8a536a877d5b9c9dffe31e701e3b
Savio <pauloluizsavio@gmail.com>
2021-09-30 14:00:18 -0300

    Criar Batalha

---
+++ src/br/com/zup/Batalha.java
@@ -0,0 +1,9 @@
+package br.com.zup;
+
+public class Batalha {
+    private Long id;
+    private Pokemon pokemonTreinador1;
+    private Pokemon pokemonTreinador2;
+    private Pokemon vencedor;
+}
+
