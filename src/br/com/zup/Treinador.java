af41c7c49f220d49b8273fb6c1f4baa8cab6fae8
Savio <pauloluizsavio@gmail.com>
2021-09-30 13:59:33 -0300

    Criar Treinador

---
+++ src/br/com/zup/Treinador.java
@@ -0,0 +1,14 @@
+package br.com.zup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Treinador {
+    private String nome;
+    private String cidade;
+    private List<Time> times = new ArrayList<>();
+}
+
+
+
+
