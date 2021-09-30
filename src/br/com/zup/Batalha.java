99fe01ba4924ed04dc435572056a378bcb6895e0
Savio <pauloluizsavio@gmail.com>
2021-09-30 14:02:47 -0300

    Adicionar getter e setter batalha

--- src/br/com/zup/Batalha.java
+++ src/br/com/zup/Batalha.java
@@ -5,5 +5,44 @@ public class Batalha {
     private Pokemon pokemonTreinador1;
     private Pokemon pokemonTreinador2;
     private Pokemon vencedor;
+
+    public Batalha(Long id, Pokemon pokemonTreinador1, Pokemon pokemonTreinador2, Pokemon vencedor) {
+        this.id = id;
+        this.pokemonTreinador1 = pokemonTreinador1;
+        this.pokemonTreinador2 = pokemonTreinador2;
+        this.vencedor = vencedor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Pokemon getPokemonTreinador1() {
+        return pokemonTreinador1;
+    }
+
+    public void setPokemonTreinador1(Pokemon pokemonTreinador1) {
+        this.pokemonTreinador1 = pokemonTreinador1;
+    }
+
+    public Pokemon getPokemonTreinador2() {
+        return pokemonTreinador2;
+    }
+
+    public void setPokemonTreinador2(Pokemon pokemonTreinador2) {
+        this.pokemonTreinador2 = pokemonTreinador2;
+    }
+
+    public Pokemon getVencedor() {
+        return vencedor;
+    }
+
+    public void setVencedor(Pokemon vencedor) {
+        this.vencedor = vencedor;
+    }
 }
 
