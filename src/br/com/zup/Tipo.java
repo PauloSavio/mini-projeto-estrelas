a9a5f7a11ebe3f8bb990aa48537d98b41a9ac1ef
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-09-30 16:19:58 -0300

    Incluir toString na classe Treinador

--- src/br/com/zup/Tipo.java
+++ src/br/com/zup/Tipo.java
@@ -3,4 +3,11 @@ package br.com.zup;
 public enum Tipo {
     FOGO, TERRA, AGUA;
 
+
+    @Override
+    public String toString() {
+        StringBuilder retornoDados = new StringBuilder();
+        retornoDados.append("Tipo: " + Tipo);
+        return retornoDados.toString();
+    }
 }
